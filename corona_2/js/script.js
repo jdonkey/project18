86786b94fc37e5e8edab4a0e9f7a6fb8ca2230eb
Roman <romamakarovgold@gmail.com>
2023-03-02 16:40:33 +0300

    user survey

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,2 +1,17 @@
 'use strict';
-const a = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);		
