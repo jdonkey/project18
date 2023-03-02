592867f562de3617560d28bab45426877465be2b
Roman <romamakarovgold@gmail.com>
2023-03-02 17:21:24 +0300

    switch

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -14,4 +14,14 @@ const a = prompt('Один из последних просмотренных ф
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);		
+console.log(personalMovieDB);
+
+const num = 5;
+
+if (num < 49) {
+	console.log('Error');
+} else if (num > 100) {
+	console.log('Много');
+} else {
+	console.log('OK!');
+}
