edfebeb5c4f92565b97774d888044330c7e77cd3
Roman <romamakarovgold@gmail.com>
2023-03-05 13:23:43 +0300

    cycle

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,35 +1,56 @@
 'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посотрели?', '');
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?', '');
+//const hamburger = 0;
+//const fries = 5;
+//if (hamburger && fries) {
+//	console.log('Я сыт');
+//}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
+// console.log(hamburger === 3 && cola && fries);
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'fdfhfuehoef');
 
+// if (hamburger === 3 && cola === 1 && fries) {
+// 	console.log('Все сыты');
+// } else {
+// 	console.log('Мы уходим');
+// }
 
-const num = 50;
-switch (num) {
-	case 49:
-		console.log('Неверно');
-		break;
-	case 1000:
-		console.log('Неверно');
-		break;
-	case 50:
-		console.log('В точку');
-		break;
-	default:
-		console.log('Не в этот раз');
-		break;
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log('Все сыты');
+// } else {
+// 	console.log('Мы уходим');
+// }
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+let num = 77;
+// while (num < 83) {
+// 	console.log(num);
+// 	num++;
+// }
+// do {
+// 	console.log(num);
+// 	num++;
+// }
+// while (num < 83);
+
+for (let i = 4; i < 12; i++) {
+	if (i === 9) {
+		// break;
+		continue;
+	}
+	console.log(i);
 }
