aa6c26257dc8c089b800ae4ee2a3525c2aa3b368
Roman <romamakarovgold@gmail.com>
2023-03-06 13:49:19 +0300

    practice conditions and cycles

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,21 +1,34 @@
 'use strict';
-let result = '';
-const length = 8;
-for (let i = 3; i < length; i++) {
-	for (let j = 0; j < i; j++) {
-		result += '*';
-	}
-	result += '\n';
-}
-console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-first: for (let i = 0; i < 3; i++) {
-	console.log(`First level: ${i}`);
-	for (let j = 0; j < 3; j++) {
-		console.log(`Second level: ${j}`);
-		for (let k = 0; k < 3; k++) {
-			if (k === 2) continue first;
-			console.log(`Third level: ${k}`);
-		}
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
 	}
+
 }
+
+if (personalMovieDB.count < 10) {
+	console.log('Вы посмотрели слишком мало!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log('Вы посмотрели норм!');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноманьяк!');
+} else {
+	console.log('ERROR!');
+}
+console.log(personalMovieDB);		
