a1772e725cfe6bb0dd20bb9fa1a420afb38f3926
Roman <romamakarovgold@gmail.com>
2023-03-06 11:36:59 +0300

    cycle in cycle

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,56 +1,10 @@
 'use strict';
-//const hamburger = 0;
-//const fries = 5;
-//if (hamburger && fries) {
-//	console.log('Я сыт');
-//}
-
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 1;
-
-// console.log(hamburger === 3 && cola && fries);
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'fdfhfuehoef');
-
-// if (hamburger === 3 && cola === 1 && fries) {
-// 	console.log('Все сыты');
-// } else {
-// 	console.log('Мы уходим');
-// }
-
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
-
-// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-// 	console.log('Все сыты');
-// } else {
-// 	console.log('Мы уходим');
-// }
-// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
-
-// let johnReport, alexReport, samReport, mariaReport = 'done';
-// console.log(johnReport || alexReport || samReport || mariaReport);
-
-let num = 77;
-// while (num < 83) {
-// 	console.log(num);
-// 	num++;
-// }
-// do {
-// 	console.log(num);
-// 	num++;
-// }
-// while (num < 83);
-
-for (let i = 4; i < 12; i++) {
-	if (i === 9) {
-		// break;
-		continue;
+let result = '';
+const length = 8;
+for (let i = 3; i < length; i++) {
+	for (let j = 0; j < i; j++) {
+		result += '*';
 	}
-	console.log(i);
+	result += '\n';
 }
+console.log(result);
