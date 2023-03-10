3ed962f9beddfda4007ec5c96a102fef2ffe0df5
Roman <romamakarovgold@gmail.com>
2023-03-10 17:10:01 +0300

    Spread

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,53 +1,95 @@
 'use strict';
-function first() {
-	setTimeout(function () {
-		console.log(1);
-	}, 500);
+const arr = [1, 24, 23, 6, 8];
+arr.sort(compareNum);
+function compareNum(a, b) {
+	return a - b;
 }
-function second() {
-	console.log(2);
-}
-first();
-second();
+console.log(arr);
+// arr.pop();
+// arr.push(222);
+// arr[99] = 22;
+// console.log(arr.length);
+// console.log(arr);
+arr.forEach(function (item, i, arr) {
+	console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(i);
+// }
+// for (let value of arr) {
+// 	console.log(value);
+// }
+const str = prompt('', '');
+const products = str.split(', ');// на основании строк, формируем массив.
+arr.sort();//сортирует массив как строки
+console.log(products.join('; ')); //на основании массива, делаем строку
 
-function learnJS(lang, callback) {
-	console.log(`Я учу ${lang}`);
-	callback();
-}
-const wellDone = function () {
-	console.log('Я прошел этот урок!!!!!');
-}
-learnJS('JavaScript', wellDone);
+const obj = {
+	a: 5,
+	b: 1
+};
+// const copy = obj;
+// copy.a = 12;
 
-const options = {
-	name: 'test',
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: 'black',
-		bg: 'red',
-	},
-	makeTest: function () {
-		console.log('тест');
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+	let objCopy = {};
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+	return objCopy;
+}
+
+const numbers = {
+	a: 3,
+	b: 'w',
+	f: {
+		g: 33,
+		h: 6
 	}
 };
-options.makeTest();
 
-// Деструктуризация объекта
-const { bg, border } = options.colors;
-console.log(bg);
+const add = {
+	d: 17,
+	e: 21
+};
+const odd = Object.assign({}, add);
+odd.d = 18;
+odd.z = 30;
+console.log(add);
+console.log(odd);
 
-// console.log(Object.keys(options).length);
-// // let count = 0;
-// for (let key in options) {
-// 	if (typeof (options[key]) === 'object') {
-// 		for (let i in options[key]) {
-// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}!`);
-// 			// count++;
-// 		}
-// 	} else {
-// 		console.log(`Свойство ${key} имеет значение ${options[key]}!`);
-// 		// count++;
-// 	}
-// }
-// // console.log(count);
+const oldArr = ['w', 'rr', 3];
+const newArr = oldArr.slice();
+newArr.push(55, 'eee');
+newArr[5] = 'ewwwwww';
+
+console.log(oldArr);
+console.log(newArr);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejournal', 'bloger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'sd'];
+const newAarray = [...array];
+const q = {
+	one: 1,
+	two: 2
+}
+
+const newObbbj = { ...q };
