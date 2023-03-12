eb8cfe1233c01661044c876c050347d9e43aade4
Roman <romamakarovgold@gmail.com>
2023-03-12 15:39:38 +0300

    practice_1

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,23 +1,68 @@
 'use strict';
-let str = 'some';
-let strObj = new String(str);
 
-console.log(typeof (str));
-console.log(typeof (strObj));
 
-const hayabusa = {
-	speed: 300,
-	riders: 1,
-	sayVerryGood: function () {
-		console.log('So Good!!!');
+
+
+const personalMovieDB = {
+	count: 0,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+	start: function () {
+		personalMovieDB.count = +prompt('Сколько фильмов вы уже посотрели?', '');
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt('Сколько фильмов вы уже посотрели?', '');
+		}
+	},
+	rememberMyFilms: function () {
+		for (let i = 0; i < 2; i++) {
+			const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+
+			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+				personalMovieDB.movies[a] = b;
+				console.log('done');
+			} else {
+				console.log('error');
+				i--;
+			}
+		}
+	},
+	detectPersonalLevel: function () {
+		if (personalMovieDB.count < 10) {
+			console.log('Вы посмотрели слишком мало!');
+		} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+			console.log('Вы посмотрели норм!');
+		} else if (personalMovieDB.count > 30) {
+			console.log('Вы киноманьяк!');
+		} else {
+			console.log('ERROR!');
+		}
+	},
+	showMyDB: function (hidden) {
+		if (!hidden) {
+			console.log(personalMovieDB);
+		}
+	},
+	toggleVisibleMyDB: function () {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		} else {
+			personalMovieDB.privat = true;
+		}
+	},
+	writeYourGenres: function () {
+		for (let i = 1; i <= 3; i++) {
+			const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+			personalMovieDB.genres[i - 1] = genre;
+		}
 	}
 };
-let alfa = {
-	speed: 70
-};
-// Object.setPrototypeOf(alfa, hayabusa);
 
-const pulsar = Object.create(hayabusa);
-console.log(pulsar.speed);
-alfa = Object.create(hayabusa);
-alfa.sayVerryGood();
+
+
+
+
+
+
