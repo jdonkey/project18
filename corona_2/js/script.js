a63af51cdfada2d031be21c9177767000cb2872f
Roman <romamakarovgold@gmail.com>
2023-03-22 16:49:33 +0300

    set

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,63 +1,15 @@
 'use strict';
 
-const shops = [
-	{ rice: 500 },
-	{ oil: 200 },
-	{ bread: 50 }
-];
+const arr = ['Alex', 'Annie', 'Oleg', 'Alex'];
 
-const map = new Map([
-	[{ paper: 400 }, 8000]
-]);
-map.set(shops[0], 5000);
-map.set(shops[1], 15000);
-map.set(shops[2], 3000);
-console.log(map);
-//то же самое с помощью цикла
-const budget = [5000, 15000, 3000];
-shops.forEach((shop, i) => {
-	map.set(shop, budget[i]);
+function unique(arr) {
+	return Array.from(new Set(arr));
+}
+console.log(unique(arr));
+
+const set = new Set(arr);
+set.add('Ivan');
+console.log(set);
+set.forEach((value, valueAgain, set) => {
+	console.log(value, valueAgain);
 })
-console.log(map);
-// console.log(map.get(shops[0]));
-// console.log(map.has(shops[0]));
-
-// console.log(map.keys());
-
-//первый метод
-const goods = [];
-for (let shop of map.keys()) {
-	goods.push(Object.keys(shop)[0])
-}
-console.log(goods);
-
-//второй метод
-for (let price of map.values()) {
-	console.log(price);
-}
-
-//третий метод
-
-for (let [shop, price] of map.entries()) {
-	console.log(price, shop);
-}
-
-//четвертый метод
-map.forEach((value, key, map) => {
-	console.log(key, value);
-});
-
-//перевод объекта в карту map
-const user = {
-	name: 'Alex',
-	surname: 'Smith',
-	birthday: '20/04/1993',
-	showMyPublicData: function () {
-		console.log(`${this.name} ${this.surname}`);
-	}
-}
-const userMap = new Map(Object.entries(user));
-console.log(userMap);
-//перевод карты в объект
-const newUserObj = Object.fromEntries(userMap);
-console.log(newUserObj);
