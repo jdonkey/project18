55bfa739c0ea39373c64144af0bb44ad6842dafa
Roman <romamakarovgold@gmail.com>
2023-03-23 15:40:34 +0300

    делегирование

--- corona_2/js/script.js
+++ corona_2/js/script.js
@@ -1,15 +1,49 @@
 'use strict';
 
-const arr = ['Alex', 'Annie', 'Oleg', 'Alex'];
+const btns = document.querySelectorAll('button');
+wrapper = document.querySelector('.btn-block');
 
-function unique(arr) {
-	return Array.from(new Set(arr));
-}
-console.log(unique(arr));
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[3].classList.toggle('blue'));
+// if (btns[1].classList.contains('red')) {
+// 	console.log('red');
+// }
 
-const set = new Set(arr);
-set.add('Ivan');
-console.log(set);
-set.forEach((value, valueAgain, set) => {
-	console.log(value, valueAgain);
-})
+// btns[0].addEventListener('click', () => {
+// 	if (!btns[1].classList.contains('red')) {
+// 		btns[1].classList.add('red');
+// 	} else {
+// 		btns[1].classList.remove('red');
+// 	}
+// })
+
+btns[0].addEventListener('click', () => {
+	btns[1].classList.toggle('red');
+});
+
+//делегирование событий
+wrapper.addEventListener('click', (event) => {
+	if (event.target && event.target.tagName == 'BUTTON') {
+		console.log('Hello');
+	}
+});
+
+wrapper.addEventListener('click', (event) => {
+	if (event.target && event.target.matches('button.red')) {
+		console.log('Hello');
+	}
+});
+
+// ошибка с делегированием
+btns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		console.log('Hello');
+	});
+});
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.apend(btn);
+
